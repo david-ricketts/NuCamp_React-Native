6116882b54c08dceb375b61fb67d796d403c4771
David Ricketts <david.ricketts119@gmail.com>
2023-02-19 07:40:41 -0500

    Test Commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { CheckBox, Input, Button, Icon } from 'react-native-elements';
+import { View, ScrollView, StyleSheet, Image } from 'react-native';
+import { CheckBox, Button, Icon, Input } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as ImagePicker from 'expo-image-picker';
+import { baseUrl } from '../shared/baseUrl';
+import { logo } from '../assets/images/logo.png';
+import * as ImageManipulator from "expo-image-manipulator"
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -107,6 +111,7 @@ const RegisterTab = () => {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [remember, setRemember] = useState(false);
+    const [imageUrl, setImageUrl] = useState(baseUrl + 'images/logo.png');
 
     const handleRegister = () => {
         const userInfo = {
@@ -133,9 +138,63 @@ const RegisterTab = () => {
         }
     };
 
+    const getImageFromCamera = async () => {
+        const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
+
+        if(cameraPermission.status==='granted'){
+            const capturedImage = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [1,1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                setImageUrl(capturedImage.uri);
+            }    
+        }
+    };
+
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                // setImageUrl(capturedImage.uri);
+                processImage(capturedImage.uri);
+            }
+        }
+    }
+    const processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+                imgUri,
+                    [{
+                        resize: { width: 400 }
+                    }],
+                    {
+                        format: ImageManipulator.SaveFormat.PNG
+                    }
+                );
+            console.log(processedImage);
+            setImageUrl(processedImage.uri)
+        }
+        
+
     return (
         <ScrollView>
             <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{ uri: imageUrl }}
+                    loadingIndicatorSource={logo}
+                    style={styles.image}
+                />
+                <Button title='Camera' onPress={getImageFromCamera} />
+                <Button title='Gallery' onPress={getImageFromGallery} />
+            </View>
                 <Input
                     placeholder='Username'
                     leftIcon={{ type: 'font-awesome', name: 'user-o' }}
@@ -271,6 +330,17 @@ const styles = StyleSheet.create({
         margin: 20,
         marginRight: 40,
         marginLeft: 40
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10
+    },
+    image: {
+        width: 60,
+        height: 60
     }
 });
 
